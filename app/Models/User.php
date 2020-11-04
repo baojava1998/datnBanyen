205d4e2c43eef33136a8a62ce528ae30c7c0690d
HuynhBao <Huynhduybao98@gmail.com>
2020-11-04 10:41:15 +0700

    develop

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,16 +1,44 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    protected $table = 'users';
-    protected $primaryKey = 'id';
-    protected $fillable = ['name', 'email', 'password', 'phone'];
+    use Notifiable;
 
-    public function role() {
-        return $this->belongsTo('App\Role', 'role', 'role_id');
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function comment()
+    {
+        return $this->hasMany('App\Comment','idUser','id');
     }
 }
