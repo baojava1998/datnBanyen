a985d71b68748529fcf4409a4f401e4468611eb6
HuynhBao <baohd@tekup.vn>
2021-01-17 18:22:13 +0700

    [nguoidung]

--- routes/web.php
+++ routes/web.php
@@ -141,5 +141,4 @@ Route::group(['prefix' => 'admin', 'middleware' => 'adminLogin'], function () {
 Route::get('/home', 'ChatController@index')->name('home');
 Route::get('/mess/{ourid}', 'ChatController@getMess');
 Route::post('/mess', 'ChatController@sendMess');
-Route::get('/logout', 'UserController@getDangXuatAdmin')->name('logout');
 //Route::get('/','Controller@index');
